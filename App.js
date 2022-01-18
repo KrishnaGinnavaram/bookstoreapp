1d97a658249dfd2d5d9803115b8ee47d8b229701
krishna <krrish.109@gmail.com>
2022-01-18 17:38:24 +0530

    removed unnecessary files

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {StyleSheet, View } from "react-native";
 import AddBooksToBookStore from "./bookstoreapp/AddBooksToBookStore";
+import BottomTabNavigator from "./bookstoreapp/BottomTabNavigator";
+import DashboardView from "./bookstoreapp/DashboardView";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <AddBooksToBookStore />
+      {/* <AddBooksToBookStore /> */}
+      <BottomTabNavigator />
     </View>
   )
 }
--- bookstoreapp/BottomTabNavigator.tsx
+++ bookstoreapp/BottomTabNavigator.tsx
@@ -3,13 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Icon } from 'react-native-elements';
 import DashboardView from './DashboardView';
+import LoginForm from './LoginForm';
 function BottomTabNavigator(props) {
    const Tab = createBottomTabNavigator();
     return (
         <NavigationContainer>
             <Tab.Navigator tabBarOptions={{activeTintColor: "blue",inactiveTintColor: "gray"}}>
-                <Tab.Screen name="Home" component={DashboardView} options=
+            <Tab.Screen name="Home" component={LoginForm} options=
                 {{tabBarIcon: ({color}) => (<Icon name="home" color={color} tvParallaxProperties={undefined} />)}}
+                />
+                <Tab.Screen name="Books" component={DashboardView} options=
+                {{tabBarIcon: ({color}) => (<Icon name="library-books" color={color} tvParallaxProperties={undefined} />)}}
                 />
             </Tab.Navigator>
         </NavigationContainer>
